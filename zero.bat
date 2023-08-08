0ceed97cdb19ce0d8e868817fd03d1fe2731c47c
mahdi hosseini <hosseinim018@gmail.com>
2023-08-08 13:07:44 +0330

    add rename file or dirctory

--- zero.bat
+++ zero.bat
@@ -18,14 +18,25 @@ if "%~1" equ "create" (
 		)
 	)		
 )
+
+REM Check if the command is "zero delete -f <filename> or -d <dir>"
 if "%~1" equ "delete" (
+    REM Check if the command is "zero delete -f <filename>"
     if "%~2" equ "-f" (
         del %~3
         echo File %~3 Delete
     )
+    REM Check if the command is "zero delete -d <dir>"
     if "%~2" equ "-d" (
         rmdir %~3
         echo Directory %~3 Deleted
+    )
+)
+
+REM Check if the command is "zero rename <filename> or <dir> to <filename> or <dir>"
+if "%~1" equ "rename" (
+    if "%~3" equ "to" (
+        RENAME  %~2 %~4
     )
 )
 
